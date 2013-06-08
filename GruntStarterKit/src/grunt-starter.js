8a0da5086f63fa562b78429a4394781a47acc5af
Alan Brown <alan@ajbis.co.uk>
2013-06-08 19:14:29 +0100

    Added grunt folder (minus modules)

---
+++ GruntStarterKit/src/grunt-starter.js
@@ -0,0 +1,9 @@
+(function(){
+	window.GruntStarter = {}
+	app = window.GruntStarter;
+	app.prototype.init = function(){
+		var helloWorld = "Hello World!";
+		alert(helloWorld);
+	}
+	app.init();
+})();
